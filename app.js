1604799f49b6f5aa313380e9f48bc3b36d44e7e6
sj1218305 <jainshobit0@gmail.com>
2021-06-26 15:34:58 +0530

    init changes

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Shobhit:Jokers123@@cluster0.0kqcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.send('home'));
+app.get('/smoothies', (req, res) => res.send('smoothies'));
